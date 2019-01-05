e89785d0594578ab2f4bfed7eb099fbdfe43d67d
Brandon Teich <brandonteich123@gmail.com>
2019-01-05 02:06:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var message = ` is ${height} inches tall`
 module.exports = {name, height, message}
 
 var name = "Susan"
+<<<<<<< HEAD
 var height = 34
+=======
+var height = 39
+>>>>>>> 2ad86021d6853a365fa29c6fa593d20d83e74659
 var message = ` ${name} is ${height} inches tall`
 module.exports = {name, height, message}
